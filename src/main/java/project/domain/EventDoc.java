a9f87b9b6ee8c0ac330bf4197353069c58d92c3a
swa07016 <swa07016@naver.com>
2021-03-29 13:42:20 +0000

    Design location, title and tab

--- src/main/java/project/domain/EventDoc.java
+++ src/main/java/project/domain/EventDoc.java
@@ -1,23 +1,23 @@
-package project.domain;
+// package project.domain;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+// import org.springframework.data.annotation.Id;
+// import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Setter;
+// import lombok.Setter;
 
 
-@Setter
-public class EventDoc {
+// @Setter
+// public class EventDoc {
     
-    @Id
-    private String _id;
+//     @Id
+//     private String _id;
     
-    public String title;
+//     public String title;
     
-    public void setTitle(String _title) {
-        this.title = _title;
-    }
+//     public void setTitle(String _title) {
+//         this.title = _title;
+//     }
     
-    private String image;
-}
+//     private String image;
+// }
 
